e7e68728e90ae026c7ab0428e125aaf0af78b85d
jonathan.heindl <jonathan.heindl@brandad-systems.de>
2018-12-20 12:29:02 +0100

    import for Javascriptinterface

--- android/src/main/java/com/flutter_webview_plugin/JavascriptInterface.java
+++ android/src/main/java/com/flutter_webview_plugin/JavascriptInterface.java
@@ -1,22 +1,22 @@
 package com.flutter_webview_plugin;
 
 import android.content.Context;
-
+import android.webkit.JavascriptInterface;
 import java.util.HashMap;
 import java.util.Map;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 
-public class JavascriptInterface {
+public class CustomJavascriptInterface {
     Context mContext;
     MethodChannel channel;
 
-    JavascriptInterface(Context c, MethodChannel channel) {
+    CustomJavascriptInterface(Context c, MethodChannel channel) {
         mContext = c;
         this.channel=channel;
     }
     @JavascriptInterface
-    void message(String message,String type) {
+    public void message(String message,String type) {
         Map<String, Object> messageContainer = new HashMap<>();
         messageContainer.put("type", type);
         messageContainer.put("message",message);
